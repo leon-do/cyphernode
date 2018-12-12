cc850f0dfb86c18c4624aa22d7c908071332c1f7
jash <jash@schulterklopfer-productions.de>
2018-12-12 13:31:28 +0100

    added support for grafana docker container

--- install/generator-cyphernode/generators/app/templates/installer/config.sh
+++ install/generator-cyphernode/generators/app/templates/installer/config.sh
@@ -3,6 +3,7 @@ BITCOIN_INTERNAL=<%= (bitcoin_mode==="internal"?'true':'false') %>
 FEATURE_TRAEFIK=<%= (features.indexOf('traefik') != -1)?'true':'false' %>
 FEATURE_LIGHTNING=<%= (features.indexOf('lightning') != -1)?'true':'false' %>
 FEATURE_OTSCLIENT=<%= (features.indexOf('otsclient') != -1)?'true':'false' %>
+FEATURE_GRAFANA=<%= (features.indexOf('grafana') != -1)?'true':'false' %>
 LIGHTNING_IMPLEMENTATION=<%= lightning_implementation %>
 PROXY_DATAPATH=<%= proxy_datapath %>
 GATEKEEPER_DATAPATH=<%= gatekeeper_datapath %>
@@ -17,6 +18,9 @@ LIGHTNING_DATAPATH=<%= lightning_datapath %>
 <% } %>
 <% if ( features.indexOf('otsclient') !== -1 ) { %>
 OTSCLIENT_DATAPATH=<%= otsclient_datapath %>
+<% } %>
+<% if ( features.indexOf('grafana') !== -1 ) { %>
+GRAFANA_DATAPATH=<%= grafana_datapath %>
 <% } %>
 <% if ( bitcoin_mode==="internal" ) { %>
 BITCOIN_DATAPATH=<%= bitcoin_datapath %>
